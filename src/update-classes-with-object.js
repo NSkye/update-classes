364c2216890996687d86159acee42511af7af6da
NSkye <nikolai.laevskii@yandex.ru>
2020-05-15 04:36:25 +0300

    Fix iteration over passed css-classes

--- src/update-classes-with-object.js
+++ src/update-classes-with-object.js
@@ -2,7 +2,7 @@ const recognizeAction = require('./recognize-action')
 
 const updateClassesWithObject = (targets, classes) => {
   targets.forEach(({ classList }) => {
-    for (className in classes) {
+    for (const className in classes) {
       const action = classes[className]
       const { method, args } = recognizeAction(action, className)
       classList[method](...args)
