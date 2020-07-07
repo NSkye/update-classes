9ba02d56bdfdc807dbc7367a82b0d9f80730c19d
NSkye <nikolai.laevskii@yandex.ru>
2020-07-07 17:15:15 +0300

    Code style for webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ['babel-loader', 'eslint-loader'],
+        use: [ 'babel-loader', 'eslint-loader' ],
       },
     ],
   },
